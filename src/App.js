2243c794f14e120507edf1afc70b159f9f3f63bb
JolantaWro <jolanta.woronowska@gmail.com>
2024-09-03 13:17:50 +0200

    organize folders

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-fuchsia-800">
-      Init project
-    </h1>
+      
     </div>
   );
 }
