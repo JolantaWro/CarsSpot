c4b69d8e454ca81278bd6c360840c419b28cc8c4
JolantaWro <jolanta.woronowska@gmail.com>
2024-09-04 10:54:49 +0200

    create header for mobile

---
+++ src/components/Header/HeaderMobile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { HashLink as Link } from "react-router-hash-link";
+
+const HeaderMobile = () => {
+    return (
+        <nav className='py-10'>
+            <ul className="flex gap-x-6 justify-center items-center ">
+                <li className=''>
+                    <Link smooth to="#galery" className="font-robotoFlex text-base font-medium leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4 hover:text-cobalt">
+                        Galeria zdjęć
+                    </Link>
+                </li>
+                <li className=''>
+                    <Link smooth to="#fqa" className="font-robotoFlex text-base font-medium leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4 hover:text-cobalt">
+                        FaQ
+                    </Link>
+                </li>
+                <li className=''>
+                    <Link smooth to="#kontakt" className="font-robotoFlex text-base font-medium leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4 hover:text-cobalt">
+                        Zadzwoń do nas
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+
+    )
+}
+  
+export default HeaderMobile;  
--- src/components/Header/HeaderNavigation.js
+++ src/components/Header/HeaderNavigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { HashLink as Link } from "react-router-hash-link";
 
 const HeaderNavigation = () => {
@@ -7,19 +6,18 @@ const HeaderNavigation = () => {
         <nav>
             <ul className="flex gap-x-6">
                 <li>
-                    <Link smooth to="#galery" className="font-robotoFlex text-base font-normal leading-6">
+                    <Link smooth to="#galery" className="font-robotoFlex text-base font-normal leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4">
                         Galeria zdjęć
                     </Link>
                 </li>
                 <li>
-                    <Link smooth to="#fqa" className="font-robotoFlex text-base font-normal leading-6">
+                    <Link smooth to="#fqa" className="font-robotoFlex text-base font-normal leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4">
                         FaQ
                     </Link>
                 </li>
             </ul>
         </nav>
-
     )
 }
   
-  export default HeaderNavigation;  
+export default HeaderNavigation;  
