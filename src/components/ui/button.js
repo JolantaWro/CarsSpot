c3a477d72a4551d7ec80fa55c45ca6019510cef5
JolantaWro <jolanta.woronowska@gmail.com>
2024-09-04 20:43:01 +0200

    create hero section for mobile

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({ children, href, secondary, ...props }) => {
+const Button = ({ children, href, extraStyles, secondary, ...props }) => {
 
-  const buttonStyles = "font-robotoFlex text-[15px] font-semibold leading-[22px] rounded-lg py-3 px-6 h-[47px]";
+  const buttonStyles = `font-robotoFlex text-[15px] font-semibold leading-[22px] rounded-lg py-3 px-6 h-[47px] ${extraStyles}`;
   const primaryStyles = "bg-white text-cobalt border hover:bg-cobalt hover:text-white";
   const secondaryStyles = "bg-cobalt text-gray hover:bg-white hover:text-cobalt hover:border";
 
