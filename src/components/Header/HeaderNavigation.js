a5366b121a0d13ba8aac4a1aceec7d5049fe529f
JolantaWro <jolanta.woronowska@gmail.com>
2024-09-09 22:07:40 +0200

    add hover in navigation link

--- src/components/Header/HeaderNavigation.js
+++ src/components/Header/HeaderNavigation.js
@@ -6,12 +6,12 @@ const HeaderNavigation = () => {
         <nav className='z-10'>
             <ul className="flex gap-x-6">
                 <li>
-                    <Link smooth to="#gallery" className="font-robotoFlex text-base font-normal leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4">
+                    <Link smooth to="#gallery" className="font-robotoFlex text-base font-normal leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4 hover:text-cobalt">
                         Galeria zdjęć
                     </Link>
                 </li>
                 <li>
-                    <Link smooth to="#faq" className="font-robotoFlex text-base font-normal leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4">
+                    <Link smooth to="#faq" className="font-robotoFlex text-base font-normal leading-6 transform transition-transform hover:scale-105 hover:underline hover:underline-offset-4 hover:text-cobalt">
                         FaQ
                     </Link>
                 </li>
